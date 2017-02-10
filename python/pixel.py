87db8fad2ef5bdf68ad5fa1881b279b9a40d55b2
AlexKorvin <aleksey.frundin@gmail.com>
2017-02-10 16:08:29 +0200

    бьюсь над решением отловить нажатие клавишы и вызвать функцию

--- python/pixel.py
+++ python/pixel.py
@@ -1,4 +1,27 @@
 import pyautogui
+import msvcrt
+import pyHook
+import pythoncom
+import ctypes
 
-x,y=pyautogui.position()
-pyautogui.displayMousePosition()
+def get_pixel():
+    x,y=pyautogui.position()
+    return pyautogui.pixel(x,y)
+
+def OnKeyboardEvent(event):
+    if event.Key=='backspace':
+        return true
+    else: False
+
+mod=pyHook.HookManager()
+mod.KeyAll=OnKeyboardEvent
+mod.HookKeyboard()
+pythoncom.PumpMessages(
+while True:
+    #print(msvcrt.getch())
+    # if mod.HookKeyboard():
+    #     pyautogui.moveTo(300,100)
+    #     pyautogui.click(1)
+    #     pyautogui.typewrite("Hello")
+        # '''print('yes')'''
+    print(mod.HookKeyboard())
