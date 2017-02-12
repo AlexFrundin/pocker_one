9d4d3b4cf8ec5b5fea6af5c5ba794892d5d6672e
AlexFrundin <aleksey.frundin@gmail.com>
2017-02-12 12:47:48 +0200

    Проверка на сравнение

---
+++ test_istuple.py
@@ -0,0 +1,10 @@
+import pyautogui
+import os
+import time
+rgb=(0,0,0)
+while True:
+    x,y=pyautogui.position()
+    if rgb==pyautogui.pixel(x,y):
+        print("excellent")
+        time.sleep(1)
+        os.system('cls')
