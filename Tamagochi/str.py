be297db1de8b90842c297e9726098619de35ce31
AlexKorvin <aleksey.frundin@gmail.com>
2017-02-23 12:56:34 +0200

    игра

---
+++ Tamagochi/str.py
@@ -0,0 +1,11 @@
+str_all="-"*40
+str_bend="-"+" "*38+"-"
+level=[
+            str_all,
+            str_bend,str_bend,str_bend,str_bend,str_bend,str_bend,str_bend,str_bend,str_bend,str_bend,
+            str_bend,str_bend,str_bend,str_bend,str_bend,str_bend,str_bend,str_bend,str_bend,str_bend,
+            str_bend,str_bend,str_bend,str_bend,str_bend,str_bend,str_bend,str_bend,str_bend,str_bend,
+            str_all
+    ]
+for i in level:
+    print(i)
