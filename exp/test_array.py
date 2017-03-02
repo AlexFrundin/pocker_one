8e1ad9bd6bcfffa3abc448897b0bedef0e1e24f9
AlexKorvin <aleksey.frundin@gmail.com>
2017-03-02 19:21:38 +0200

    немного изменил принцип срезающего механизма

--- exp/test_array.py
+++ exp/test_array.py
@@ -1,6 +1,6 @@
 expans=[]
 
-def func(temp,lst):
+'''def func(temp,lst):
     temp=[]
     for i in lst:
         temp.append(i)
@@ -16,4 +16,15 @@ func(temp,lst)
 
 print(expans)
 expans.reverse()
-print(expans)
+print(expans)'''
+
+
+def alternate_func(lst):
+    if len(lst)==0:
+        return
+    expans.append(lst)
+    alternate_func(lst[:-1])
+lst=[1,2,3,4,5,6]
+alternate_func(lst)
+for item in expans:
+    print(item)
