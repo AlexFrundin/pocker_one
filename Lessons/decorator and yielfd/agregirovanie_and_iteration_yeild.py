62670bcce7e2345079569f342c0504165151b8d9
AlexKorvin <aleksey.frundin@gmail.com>
2017-03-09 09:33:46 +0200

    Изменена структура папок

---
+++ Lessons/decorator and yielfd/agregirovanie_and_iteration_yeild.py
@@ -0,0 +1,28 @@
+class MyList:
+    def __init__(self,l=[]):
+        #делает копию контейнера
+        self._l=list(l)
+    def __repr__(self):
+        return repr(self._l)
+    def __len__(self):
+        return len(self._l)
+    #важный метод для контейнера оператор in
+    def __contains__(self,value):
+        return value in self._l
+    def __bool__(self):
+        return bool(self._l)
+    def add(self,value):
+        self._l.append(value)
+    #индексирование элементов
+    def __setitem__(self,index,value):
+        self._l[index]=value
+    def __getitem__(self,index):
+        if isinstance(index,tuple):
+            return [self._l[i] for i in index]
+        elif index==Ellipsis:
+            #для инкапсуляции собственной коллекции
+            return self._l.copy()
+        return self._l[index]
+    def __iter__(self):
+        for i in self._l:
+            yield i
